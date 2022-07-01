70a569702a8b3e2d60c0388759e097ff4ba84958
Swsan3 <108540540+Swsan3@users.noreply.github.com>
2022-07-01 18:20:22 +0200

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Test()
+{
+	"use strict"
+	var
+		Email = document.getElementById("email").value,
+		Pass = document.getElementById("password").value;
+		
+		console.log("You Email " + Email, " Your Password " + Pass);
+		
+}
